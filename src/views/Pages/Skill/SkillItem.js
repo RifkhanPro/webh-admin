bf04e08d0e0d429c2d45be57dd862e7a591cd055
RaveenS20 <raveen@prodigit.in>
2022-12-28 23:12:00 +0530

    skill view image removed

--- src/views/Pages/Skill/SkillItem.js
+++ src/views/Pages/Skill/SkillItem.js
@@ -1,7 +1,7 @@
 import React from "react"
 import { useNavigate } from "react-router-dom"
 import { Card, CardHeader, CardBody, CardTitle, CardText, CardLink } from 'reactstrap'
-import avatar from './../../../assets/images/pages/not_available.jpg'
+// import avatar from './../../../assets/images/pages/not_available.jpg'
 import './SkillItem.css'
 const SkillItem = (props) => {
 
@@ -14,11 +14,11 @@ const SkillItem = (props) => {
   return ( 
           <div className="skill_card" onClick={() => routehandler(props.id)}>
             <div className="title">{props.title}</div>
-            <div className="image">
-              {!props.image && <img src={avatar} style={{ height:'230px', width:'250px'}} />}
-              {props.image && <img src={props.image} alt="" />}
-            </div>
-            <div className="desc">{props.desc}</div>
+            {/* <div className="image"> */}
+              {/* {!props.image && <img src={avatar} style={{ height:'230px', width:'250px'}} />} */}
+              {/* {props.image && <img src={props.image} alt="" />} */}
+            {/* </div> */}
+            {/* <div className="desc">{props.desc}</div> */}
           </div>
         )
   
